892e5b23b97dc844b52bb61b3fc76c393c04236e
Clemens <clemens.bauer@htl-klu.at>
2018-07-10 12:10:34 +0200

    Added FileChooser

--- TestProjekt/src/main/java/com/github/mitschi/App.java
+++ TestProjekt/src/main/java/com/github/mitschi/App.java
@@ -8,12 +8,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class App extends Application {
+    protected Stage stage;
+    @FXML protected TextField textFieldPath;
+
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(new Scene(root, 800, 600));
@@ -30,6 +40,14 @@ public class App extends Application {
         Alert alert = new Alert(Alert.AlertType.INFORMATION, "Hello World!");
         alert.show();
 
+    }
+
+    @FXML
+    protected void choosePath(ActionEvent event){
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(stage);
+
+        textFieldPath.setText(file.getPath());
     }
 
 }

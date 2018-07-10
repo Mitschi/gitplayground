cab9a55694453837e446fdf499d4de12e81dc001
Clemens <clemens.bauer@htl-klu.at>
2018-07-10 09:51:21 +0200

    +printDoubleArray();

---
+++ Name/src/MyObject.java
@@ -0,0 +1,31 @@
+import java.util.Objects;
+
+public class MyObject {
+    private int length;
+
+    public MyObject(int length){
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyObject myObject = (MyObject) o;
+        return length == myObject.length;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(length);
+    }
+}

29b9912f00bc8071603ba1daa0e3f2c86c46acc1
Jana <yana.n.friedrich@gmail.com>
2018-07-10 09:20:49 +0200

    Methodenaufruf

--- Name/src/Main.java
+++ Name/src/Main.java
@@ -5,9 +5,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.print("Anders");
+        int[] array = {1,2,3};
+        printArray(array);
     }
 
-    public void printArray(int[] array){
+    public static void printArray(int[] array){
         System.out.println(Arrays.toString(array));
     }
 }

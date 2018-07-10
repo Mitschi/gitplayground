5f8297f2c918941e23cbde6017fa6f4d6ac67866
Jana <yana.n.friedrich@gmail.com>
2018-07-10 09:25:15 +0200

    add();

--- Name/src/Main.java
+++ Name/src/Main.java
@@ -7,9 +7,19 @@ public class Main {
         System.out.print("Anders");
         int[] array = {1,2,3};
         printArray(array);
+
+        int a= 10;
+        int b= 210;
+
+        add(a,b);
     }
 
     public static void printArray(int[] array){
         System.out.println(Arrays.toString(array));
+    }
+
+    public static void add (int a, int b){
+        System.out.print(a+b);
+
     }
 }

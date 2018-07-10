0a8bc0b102bd4a3edda8e92177e576be6fb6d93b
Clemens <clemens.bauer@htl-klu.at>
2018-07-10 12:14:23 +0200

    Changed FileChooser to DirectoryChooser

--- TestProjekt/src/main/java/com/github/mitschi/App.java
+++ TestProjekt/src/main/java/com/github/mitschi/App.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser;
 
@@ -44,8 +45,8 @@ public class App extends Application {
 
     @FXML
     protected void choosePath(ActionEvent event){
-        FileChooser fileChooser = new FileChooser();
-        File file = fileChooser.showOpenDialog(stage);
+        DirectoryChooser fileChooser = new DirectoryChooser();
+        File file = fileChooser.showDialog(stage);
 
         textFieldPath.setText(file.getPath());
     }

16cb3bf87dacb28af2fffc47c970de8d0c301ca4
jana <yana.n.friedrich@gmail.com>
2018-07-31 10:30:12 +0200

    Refactored code in Process.java

--- TestProjekt/src/main/java/com/github/mitschi/Process.java
+++ TestProjekt/src/main/java/com/github/mitschi/Process.java
@@ -5,32 +5,23 @@ import at.aau.RepairListener;
 import at.aau.Repair;
 import at.aau.building.BuildDuration;
 import at.aau.building.BuildLog;
-import at.aau.entity.BuildResult;
 import javafx.application.Platform;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.concurrent.Task;
 import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import org.reactfx.util.FxTimer;
-import scala.util.parsing.combinator.testing.Str;
-
 import java.io.*;
 import java.text.ParseException;
 import java.util.List;
 import java.util.Objects;
 
-public class Process implements RepairListener{
+public class Process implements RepairListener {
     private String filePath;
     private Tab processTab;
     private TableView<TableRow> table;
@@ -40,7 +31,6 @@ public class Process implements RepairListener{
     private double progress;
     private BooleanProperty isRunning;
     private Stage stage;
-    private String logBuffer;
     private int currentStep;
     private String logPath;
     private BufferedWriter bw;
@@ -53,7 +43,6 @@ public class Process implements RepairListener{
     private javafx.scene.control.TableColumn buildResult;
 
     private javafx.scene.control.TableColumn button;
-
 
 
     public Repair getRepair() {
@@ -84,7 +73,8 @@ public class Process implements RepairListener{
         this.progress = progress;
     }
 
-    public Process(String filePath, Stage stage, String logPath){
+    public Process(String filePath, Stage stage, String logPath) {
+
         logExists = true;
         isRunning = new SimpleBooleanProperty(false);
         currentStep = 0;
@@ -100,6 +90,7 @@ public class Process implements RepairListener{
         repair.addRepairListener(this);
         this.filePath = filePath; // Set Filepath
         progressBar = new ProgressBar(); // initialize progressBar
+
         // initialize Table
         table = new TableView<TableRow>();
         table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
@@ -111,6 +102,7 @@ public class Process implements RepairListener{
                 App.tabListener.tabOnClose();
             }
         });
+
         // Set Table Columns
         step = new TableColumn();
         strategies = new TableColumn();
@@ -120,6 +112,7 @@ public class Process implements RepairListener{
         strategies.setText("Strategies");
         buildResult.setText("Build Result");
         button.setText("Log Windows");
+
         // add table columns
         table.getColumns().addAll(step, strategies, buildResult, button);
 
@@ -139,7 +132,6 @@ public class Process implements RepairListener{
         label.setTranslateX(400);
         processTab.setContent(pane); // add pane to tab
         pane.getChildren().addAll(progressBar, table, label); // add contents to pane
-
 
 
     }
@@ -170,7 +162,7 @@ public class Process implements RepairListener{
         return Objects.hash(filePath, processTab);
     }
 
-    public void addData(ObservableList list){
+    public void addData(ObservableList list) {
         // Add data to table
         table.getItems().addAll(list);
     }
@@ -184,6 +176,7 @@ public class Process implements RepairListener{
     public void repairEnded() {
 
         Platform.runLater(new Runnable() {
+
             @Override
             public void run() {
                 App.progressListener.progressFinished(Process.this); // call listener to delete Process
@@ -202,6 +195,7 @@ public class Process implements RepairListener{
 
     @Override
     public void stepStarted(int i, int i1, BuildLog buildLog) {
+
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
@@ -209,10 +203,10 @@ public class Process implements RepairListener{
                 ObservableList<TableRow> data = FXCollections.observableArrayList(new TableRow(i, null, buildLog, stage, filePath));
                 addData(data);
                 // set the Results of the lines
-                table.getItems().get(i-1).setResult(buildLog.getBuildResult().toString());
-                table.getItems().get(i-1).getLogWindow().updateResult(buildLog.getBuildResult().toString(), buildLog.getBuildDuration(), buildLog);
+                table.getItems().get(i - 1).setResult(buildLog.getBuildResult().toString());
+                table.getItems().get(i - 1).getLogWindow().updateResult(buildLog.getBuildResult().toString(), buildLog.getBuildDuration(), buildLog);
                 table.getItems().get(i).setResult("---");
-                table.getItems().get(i).getLogWindow().updateResult("---", new BuildDuration(0,0,0), new BuildLog());
+                table.getItems().get(i).getLogWindow().updateResult("---", new BuildDuration(0, 0, 0), new BuildLog());
 
                 currentStep++;
             }
@@ -221,6 +215,7 @@ public class Process implements RepairListener{
 
     @Override
     public void stepEnded(int i, int i1, BuildLog buildLog) {
+
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
@@ -254,17 +249,18 @@ public class Process implements RepairListener{
     public void newBuildLine(String line) {
 
         Platform.runLater(new Runnable() {
+
             @Override
             public void run() {
                 // write log to LogWindow
-                table.getItems().get(currentStep-1).getLogWindow().area.appendText(line+"\n");
-                table.getItems().get(currentStep-1).getLogWindow().area.requestFollowCaret();
-                table.getItems().get(currentStep-1).getLogWindow().updateLogWindow();
+                table.getItems().get(currentStep - 1).getLogWindow().area.appendText(line + "\n");
+                table.getItems().get(currentStep - 1).getLogWindow().area.requestFollowCaret();
+                table.getItems().get(currentStep - 1).getLogWindow().updateLogWindow();
 
-                if(logExists){
+                if (logExists) {
                     try {
                         // write Log in log File
-                        bw.write(line+"\n");
+                        bw.write(line + "\n");
                         bw.flush();
                     } catch (IOException e) {
                     }
@@ -276,6 +272,7 @@ public class Process implements RepairListener{
     @Override
     public void repairAborted() {
         Platform.runLater(new Runnable() {
+
             @Override
             public void run() {
                 App.progressListener.progressFinished(Process.this); // call listener to delete Process
@@ -298,36 +295,33 @@ public class Process implements RepairListener{
     }
 
 
-    public void start(File repoFolder, String revision, int max_steps, List<Class> allowedStrategies){
+    public void start(File repoFolder, String revision, int max_steps, List<Class> allowedStrategies) {
         Platform.runLater(new Runnable() {
+
             @Override
             public void run() {
                 // Initialize Step 0
                 BuildLog startLog = new BuildLog();
-                startLog.setBuildDuration(new BuildDuration(0,0,0));
+                startLog.setBuildDuration(new BuildDuration(0, 0, 0));
                 ObservableList<TableRow> data = FXCollections.observableArrayList(new TableRow(currentStep, null, startLog, stage, filePath));
                 addData(data);
                 table.getItems().get(0).setResult("---");
-                table.getItems().get(0).getLogWindow().updateResult("---", new BuildDuration(0,0,0), new BuildLog());
+                table.getItems().get(0).getLogWindow().updateResult("---", new BuildDuration(0, 0, 0), new BuildLog());
                 currentStep++;
             }
         });
 
-//repair.repair(repoFolder, revision, max_steps,"statistic", allowedStrategies);
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
                 try {
                     isRunning.setValue(true);
-                    if(repair == null){
+                    if (repair == null) {
 
-                    }else{
+                    } else {
                         // Start Repairtool
-                        repair.repair(repoFolder, revision, max_steps,"statistic", allowedStrategies);
+                        repair.repair(repoFolder, revision, max_steps, "statistic", allowedStrategies);
                     }
-
-
-
                 } catch (FileNotFoundException | ParseException e) {
                     e.printStackTrace();
                 }
@@ -336,7 +330,7 @@ public class Process implements RepairListener{
 
     }
 
-    public void cancel(){
+    public void cancel() {
         // cancel Repair
         repair.abortRepair();
     }
